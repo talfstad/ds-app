9386a80ddc61350302d6980aff716b7c48c851da
Trevor Alfstad <trevor@buildcave.com>
2015-08-31 21:55:06 -0700

    added csrf protection to all request endpoints

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,17 @@
 exports.initialize = function(app, db, login) {
 
+  var csrf = require('csurf');
+
+  app.use(csrf({
+    cookie: true
+  }));
+
   var login_routes = require('./login');
   login_routes.initialize(app, db, login);
 
-
   app.get("*", function(req, res) {
-    res.render('index' /*, { csrfToken: req.csrfToken() }*/ );
+    res.render('index', {
+      csrfToken: req.csrfToken()
+    });
   });
 }
