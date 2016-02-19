8feb2a00c5f45e219955701b83466eccd65ba2c4
Trevor Alfstad <trevor@buildcave.com>
2016-02-19 14:19:20 -0800

    added loading spinner stuff for login

--- public/assets/js/apps/user/login/login_controller.js
+++ public/assets/js/apps/user/login/login_controller.js
@@ -4,93 +4,77 @@ define(["app", "/assets/js/apps/user/login/login_view.js",
     "/assets/js/apps/user/login/models/reset_password_model.js",
     "/assets/js/common/logout/common_logout.js",
     "/assets/js/apps/moonlander/entry_point/entry_app.js",
-    "syphon"], 
-function(Moonlander, LoginView, LoginLayout, ResetPasswordView, ResetPasswordModel, Logout){
-  Moonlander.module("UserApp.Login", function(Login, Moonlander, Backbone, Marionette, $, _){
+    "syphon"
+  ],
+  function(Moonlander, LoginView, LoginLayout, ResetPasswordView, ResetPasswordModel, Logout) {
+    Moonlander.module("UserApp.Login", function(Login, Moonlander, Backbone, Marionette, $, _) {
 
-    Login.Controller = {
+      Login.Controller = {
 
-      showLayout: function(){
-        Moonlander.loginLayout = new LoginLayout();
-        Moonlander.loginLayout.render();
-        Moonlander.rootRegion.show(Moonlander.loginLayout);
-      },
-      
-      showLogin: function(){
-        //hide the modal region
-        $("#modal-region").hide();
-        $(".modal-backdrop").remove();
+        showLayout: function() {
+          Moonlander.loginLayout = new LoginLayout();
+          Moonlander.loginLayout.render();
+          Moonlander.rootRegion.show(Moonlander.loginLayout);
+        },
 
-        Moonlander.UserApp.Login.Controller.showLayout();
-        var loginView = new LoginView.showLogin({model: Moonlander.loginModel});
-        Moonlander.loginLayout.content.show(loginView);
-        
-        loginView.on("login:form:submit", function(){
-          var successLoginCallback = function(model, message, other) {
-            if(model.get("logged_in")) {
-              
-              //call domains:list from here mate!
-              Moonlander.trigger("start:moonlander");
-            } else {
-              //show invalid login error
-              loginView.showInvalidUserPassError();
-            }
-          };
-          var errorLoginCallback = function(model, message, other) {
-            //something happened on submit that is out of our control
-            //TODO: growl here or something to let the user know
-          };
+        showLogin: function() {
+          var me = this;
+          //hide the modal region
+          $("#modal-region").hide();
+          $(".modal-backdrop").remove();
 
-          var loginFormData = Backbone.Syphon.serialize(this);
-          this.model.set(loginFormData);
-                
-          if(this.model.isValid(true)) {
-            this.model.save({}, {success: successLoginCallback, error: errorLoginCallback});
-          }
-        });
-      },
-
-      showResetPassword: function(){
-        Moonlander.UserApp.Login.Controller.showLayout();
-        var resetPasswordModel = new ResetPasswordModel.ResetPasswordModel();
-
-        var resetPasswordView = new ResetPasswordView.showForgotPassword({model: resetPasswordModel});
-        Moonlander.loginLayout.content.show(resetPasswordView);
-
-
-        resetPasswordView.on("reset:form:submit", function(){
-          var successResetCallback = function(model, message, other) {
-            resetPasswordView.showCheckEmailMessage(model.attributes);
-          };
-          var errorResetCallback = function(model, message, other) {
-            //something happened on submit that is out of our control
-            //TODO: growl here or something to let the user know
-          };
-
-          var loginFormData = Backbone.Syphon.serialize(this);
-          this.model.set(loginFormData);
-                
-          if(this.model.isValid(true)) {
-            this.model.save({}, {success: successResetCallback, error: errorResetCallback});
-          }
-        });
-      },
-
-      showResetPasswordStep2: function(code){
-        Moonlander.UserApp.Login.Controller.showLayout();
-
-        var codeValid = function(){
-          var resetPasswordModel = new ResetPasswordModel.ResetPasswordModelStep2({
-            code: code
+          Moonlander.UserApp.Login.Controller.showLayout();
+          var loginView = new LoginView.showLogin({
+            model: Moonlander.loginModel
           });
+          Moonlander.loginLayout.content.show(loginView);
 
-          var resetPasswordViewStep2 = new ResetPasswordView.showForgotPasswordStep2({model: resetPasswordModel});
-          Moonlander.loginLayout.content.show(resetPasswordViewStep2);
+          loginView.on("login:form:submit", function() {
+            var successLoginCallback = function(model, message, other) {
+              model.set("alertLoading", false);
+
+              if (model.get("logged_in")) {
+
+                //call domains:list from here mate!
+                Moonlander.trigger("start:moonlander");
+              } else {
+                //show invalid login error
+                loginView.showInvalidUserPassError();
+              }
+            };
+            var errorLoginCallback = function(model, message, other) {
+              //something happened on submit that is out of our control
+              //TODO: growl here or something to let the user know
+            };
+
+            var loginFormData = Backbone.Syphon.serialize(this);
+            this.model.set(loginFormData);
+
+            if (this.model.isValid(true)) {
+              
+              this.model.set("alertLoading", true);
+
+              this.model.save({}, {
+                success: successLoginCallback,
+                error: errorLoginCallback
+              });
+            }
+          });
+        },
+
+        showResetPassword: function() {
+          Moonlander.UserApp.Login.Controller.showLayout();
+          var resetPasswordModel = new ResetPasswordModel.ResetPasswordModel();
+
+          var resetPasswordView = new ResetPasswordView.showForgotPassword({
+            model: resetPasswordModel
+          });
+          Moonlander.loginLayout.content.show(resetPasswordView);
 
 
-          resetPasswordViewStep2.on("reset:form:submit", function(){
+          resetPasswordView.on("reset:form:submit", function() {
             var successResetCallback = function(model, message, other) {
-              resetPasswordViewStep2.showSuccessfulReset();
+              resetPasswordView.showCheckEmailMessage(model.attributes);
             };
             var errorResetCallback = function(model, message, other) {
               //something happened on submit that is out of our control
@@ -99,45 +83,85 @@ function(Moonlander, LoginView, LoginLayout, ResetPasswordView, ResetPasswordMod
 
             var loginFormData = Backbone.Syphon.serialize(this);
             this.model.set(loginFormData);
-                  
-            if(this.model.isValid(true)) {
-              this.model.save({}, {success: successResetCallback, error: errorResetCallback});
+
+            if (this.model.isValid(true)) {
+              this.model.save({}, {
+                success: successResetCallback,
+                error: errorResetCallback
+              });
             }
           });
-        };
+        },
 
-        var codeInvalid = function(){
-          Moonlander.execute("show:resetPassword");
+        showResetPasswordStep2: function(code) {
+          Moonlander.UserApp.Login.Controller.showLayout();
+
+          var codeValid = function() {
+            var resetPasswordModel = new ResetPasswordModel.ResetPasswordModelStep2({
+              code: code
+            });
+
+            var resetPasswordViewStep2 = new ResetPasswordView.showForgotPasswordStep2({
+              model: resetPasswordModel
+            });
+            Moonlander.loginLayout.content.show(resetPasswordViewStep2);
+
+
+            resetPasswordViewStep2.on("reset:form:submit", function() {
+              var successResetCallback = function(model, message, other) {
+                resetPasswordViewStep2.showSuccessfulReset();
+              };
+              var errorResetCallback = function(model, message, other) {
+                //something happened on submit that is out of our control
+                //TODO: growl here or something to let the user know
+              };
+
+              var loginFormData = Backbone.Syphon.serialize(this);
+              this.model.set(loginFormData);
+
+              if (this.model.isValid(true)) {
+                this.model.save({}, {
+                  success: successResetCallback,
+                  error: errorResetCallback
+                });
+              }
+            });
+          };
+
+          var codeInvalid = function() {
+            Moonlander.execute("show:resetPassword");
+          }
+
+          var checkCodeModel = new ResetPasswordModel.Check();
+
+          //validate code
+          checkCodeModel.save({
+            code: code
+          }, {
+            success: function(data) {
+              if (data.attributes.isValid) {
+                codeValid();
+              } else {
+                codeInvalid();
+              }
+            }
+          });
+
+        },
+
+        logout: function() {
+          //fetch logout and on success redirect to login
+          Logout(function(login) {
+            if (!login.get("logged_in")) {
+              Moonlander.execute("show:login");
+            } else {
+              //error
+            }
+          })
         }
 
-        var checkCodeModel = new ResetPasswordModel.Check();
-
-        //validate code
-        checkCodeModel.save({code: code}, {
-          success:function(data){
-            if(data.attributes.isValid){
-              codeValid();
-            } else {
-              codeInvalid();
-            }
-          }
-        });
-        
-      },
-
-      logout: function(){
-        //fetch logout and on success redirect to login
-        Logout(function(login){
-          if(!login.get("logged_in")){
-            Moonlander.execute("show:login");
-          } else {
-            //error
-          }
-        })
       }
+    });
 
-    }
+    return Moonlander.UserApp.Login.Controller;
   });
-
-  return Moonlander.UserApp.Login.Controller;
-});
--- public/assets/js/apps/user/login/login_view.js
+++ public/assets/js/apps/user/login/login_view.js
@@ -1,61 +1,74 @@
 define(["app",
-        "tpl!/assets/js/apps/user/login/templates/login_view.tpl",
-        "/assets/js/common/validation.js",
-        "/assets/js/common/notification.js",
-        "canvasbg",
-        "bootstrap"
-        // "theme.utility",
-        // "theme.demo",
-        // "theme.main"
-        ],
-function(Moonlander, LoginViewTpl, Validation, Notification){
-  Moonlander.module("UserApp.Login", function(Login, Moonlander, Backbone, Marionette, $, _){
-    
-    Login.showLogin = Marionette.ItemView.extend({
-      id: "login-container",
-      className: "admin-form theme-info",
-      template: LoginViewTpl,
+    "tpl!/assets/js/apps/user/login/templates/login_view.tpl",
+    "/assets/js/common/validation.js",
+    "/assets/js/common/notification.js",
+    "canvasbg",
+    "bootstrap"
+    // "theme.utility",
+    // "theme.demo",
+    // "theme.main"
+  ],
+  function(Moonlander, LoginViewTpl, Validation, Notification) {
+    Moonlander.module("UserApp.Login", function(Login, Moonlander, Backbone, Marionette, $, _) {
 
-      initialize: function(){
-        Validation.bindView(this);
-      },
+      Login.showLogin = Marionette.ItemView.extend({
+        id: "login-container",
+        className: "admin-form theme-info",
+        template: LoginViewTpl,
 
-      events: {
-        'click #sign-in-button': 'submitLoginForm'
-      },
+        initialize: function() {
+          Validation.bindView(this);
+        },
 
-      submitLoginForm: function(e){
-        e.preventDefault();
-        this.trigger("login:form:submit");
-      },
+        events: {
+          'click #sign-in-button': 'submitLoginForm'
+        },
 
-      showInvalidUserPassError: function(){
-        $(".admin-form").removeClass("theme-info").addClass("theme-danger");
-        $(".panel").removeClass("panel-info").addClass("panel-danger");
-        Notification("Invalid Username or Password", "Please try again entering your credentials again.", "danger", "stack_bar_top");
-      },
+        modelEvents: {
+          "change:alertLoading": "alertLoading"
+        },
 
-      hideInvalidUserPassError: function(){
-        $(".theme-info").removeClass("theme-danger").addClass("theme-info");
-        $(".theme-panel").removeClass("panel-danger").addClass("panel-info");
-      },
+        alertLoading: function() {
+          if (this.model.get("alertLoading")) {
+            this.$el.find("span[data-handle='domain']").text(this.model.get("domain"));
+            this.$el.find(".alert-loading").fadeIn();
+          } else {
+            this.$el.find(".alert-loading").hide();
+          }
+        },
 
-      onDomRefresh: function(){
+        submitLoginForm: function(e) {
+          e.preventDefault();
+          this.trigger("login:form:submit");
+        },
 
-        // Init CanvasBG and pass target starting location
-        CanvasBG.init({
-          Loc: {
-            x: window.innerWidth / 2,
-            y: window.innerHeight / 3.3
-          },
-        });        
+        showInvalidUserPassError: function() {
+          $(".admin-form").removeClass("theme-info").addClass("theme-danger");
+          $(".panel").removeClass("panel-info").addClass("panel-danger");
+          Notification("Invalid Username or Password", "Please try again entering your credentials again.", "danger", "stack_bar_top");
+        },
 
-        $("body").addClass("external-page sb-l-c sb-r-c onload-check");
+        hideInvalidUserPassError: function() {
+          $(".theme-info").removeClass("theme-danger").addClass("theme-info");
+          $(".theme-panel").removeClass("panel-danger").addClass("panel-info");
+        },
 
-      }
+        onDomRefresh: function() {
 
+          // Init CanvasBG and pass target starting location
+          CanvasBG.init({
+            Loc: {
+              x: window.innerWidth / 2,
+              y: window.innerHeight / 3.3
+            },
+          });
+
+          $("body").addClass("external-page sb-l-c sb-r-c onload-check");
+
+        }
+
+      });
     });
-  });
 
-  return Moonlander.UserApp.Login;
-});
+    return Moonlander.UserApp.Login;
+  });
